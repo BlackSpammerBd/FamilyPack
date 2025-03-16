0ba74501207dcf5f9a7d15c4ce1e0d7c5391c531
BLACK SPAMMER BD <publicservicexyz7@gmail.com>
2025-03-16 15:23:10 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bsb_family_pack',
+    name='bsb_family_pack1',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
